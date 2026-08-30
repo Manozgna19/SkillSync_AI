cf856a8b1e7ccbadff412300139c08682aab64e7
VNavyaSri-1607 <sriammu1607@gmail.com>
2026-08-30 18:52:15 +0530

    application navigation and layout

---
+++ frontend/src/components/AppLayout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+
+export default function AppLayout() {
+  return (
+    <div className="min-h-screen bg-slate-50">
+      <Navbar />
+      <div className="flex">
+        <Sidebar />
+        <main className="flex-1 p-4 sm:p-6 lg:p-8 max-w-6xl mx-auto w-full">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
